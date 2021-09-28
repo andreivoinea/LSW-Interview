71eac9af06d0fb4a51a28609c0b4ce853b90da9b
Voinea Andrei <andrei_voinea33@yahoo.com>
2021-09-28 18:31:49 +0300

    Added Testing items
    Added Items for testing purposes and rearranged folders

--- Project Files/Assets/Scripts/Item.cs
+++ Project Files/Assets/Scripts/Item.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class Item : MonoBehaviour
 {
     public string Name;
-    public string Description;
+    public string description;
 
     [HideInInspector] public bool isTradable;
     [HideInInspector] public int price;
 
-    public Image equippedImage;
+    public Sprite image;
 
     [HideInInspector] public bool hasDurability;
     [HideInInspector] public int maxDurability;
     [HideInInspector] public int currentDurability;
+
+    private Image ImageComponent { get { return GetComponent<Image>();} }
+
+    private void Update()
+    {
+        if(ImageComponent.sprite!=image) ImageComponent.sprite = image;
+    }
 }
